4586ce5ed8faa2cb66174263a3e3a76b50f78c6e
jaykachhadiya22 <jaykachhadiya22@gmail.com>
2022-11-12 17:05:21 +0530

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ import 'package:state_hotel_project/locator/locator.dart';
 import 'package:state_hotel_project/ui/screens/home_view/bottam_navigaton_view.dart';
 import 'package:state_hotel_project/ui/screens/login_view/login_view.dart';
 
+// Hello
+
+
+
 var foodBox;
 
 void main() async {
